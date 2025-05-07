a3cc7bc73584873362f9610cd742ada66cf8cb0b
Saull Brandão <saull@outlook.com>
2025-05-07 09:13:47 -0300

    improve the script

--- boot.sh
+++ boot.sh
@@ -3,29 +3,36 @@
 set -e
 
 BASE_DIR="$HOME/.local/share/dev-setup"
-#
-# # Check for 'yay' and install it if not found
-# if ! command -v yay >/dev/null 2>&1; then
-#   echo "=> 'yay' not found. Installing 'yay'..."
-#   sudo pacman -S --needed --noconfirm git base-devel
-#   git clone https://aur.archlinux.org/yay.git /tmp/yay
-#   pushd /tmp/yay >/dev/null
-#   makepkg -si --noconfirm
-#   popd >/dev/null
-#   rm -rf /tmp/yay
-# fi
-#
-# # Update the system and 'yay'
-# echo "=> Updating system and 'yay' packages..."
-# yay -Syyuu --noconfirm
-#
-# echo "Cloning System Setup..."
-# rm -rf ~/.local/share/dev-setup
-# git clone "https://github.com/saullbrandao/dev-setup.git" ~/.local/share/dev-setup >/dev/null
-#
+
+# Check for 'yay' and install it if not found
+if ! command -v yay >/dev/null 2>&1; then
+  echo "=> 'yay' not found. Installing 'yay'..."
+  sudo pacman -S --needed --noconfirm git base-devel
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  pushd /tmp/yay >/dev/null
+  makepkg -si --noconfirm
+  popd >/dev/null
+  rm -rf /tmp/yay
+fi
+
+# Update the system and 'yay'
+echo "=> Updating system and 'yay' packages..."
+yay -Syyuu --noconfirm
+
+echo "Cloning System Setup..."
+rm -rf $BASE_DIR 
+git clone "https://github.com/saullbrandao/dev-setup.git" ~/.local/share/dev-setup >/dev/null
+
+cd $BASE_DIR
+
+#Change to use ssh
+git remote set-url origin git@github.com:saullbrandao/dev-setup.git
+
 echo "Copying configs..."
-source "$BASE_DIR/config.sh"
+source "config.sh"
 
 echo "Installation starting..."
-source "$BASE_DIR/install.sh"
+source "install.sh"
 
+cd $HOME
+echo "All done!"
