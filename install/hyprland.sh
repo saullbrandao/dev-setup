fe945e4f11be703724e2a04fdf35f33f8afa314b
Saull Brandão <saull@outlook.com>
2025-05-12 18:24:52 -0300

    hyprlock config

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -1,1 +1,4 @@
-yay -S --needed --noconfirm nwg-look hyprpolkitagent hyprpaper waybar pavucontrol hyprshade
+yay -S --needed --noconfirm nwg-look hyprpolkitagent hyprpaper waybar pavucontrol hyprshade udiskie hyprlock
+
+hyprshade install
+systemctl --user enable --now hyprshader.timer
