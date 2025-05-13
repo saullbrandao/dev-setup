30628ad8512ed45c46489c353ff726aa0e89c8e1
Saull Brandão <saull@outlook.com>
2025-05-13 08:38:14 -0300

    hypridle config

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -1,4 +1,4 @@
-yay -S --needed --noconfirm nwg-look hyprpolkitagent hyprpaper waybar pavucontrol hyprshade udiskie hyprlock
+yay -S --needed --noconfirm nwg-look hyprpolkitagent hyprpaper waybar pavucontrol hyprshade udiskie hyprlock hypridle
 
 hyprshade install
 systemctl --user enable --now hyprshader.timer
