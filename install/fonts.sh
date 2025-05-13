79501b4dcf662ee588fe9a2b7988423733d120ab
Saull Brandão <saull@outlook.com>
2025-05-13 15:20:44 -0300

    add fonts

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,1 +1,1 @@
-yay -S --needed --noconfirm ttf-jetbrains-mono-nerd inter-font ttf-ms-win11-auto
+yay -S --needed --noconfirm ttf-jetbrains-mono-nerd inter-font ttf-ms-win11-auto noto-fonts-cjk noto-fonts-emoji
