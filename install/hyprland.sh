8b54888d404e1a9a0caf3407b71404b7c98e33fc
Saull Brandão <saull@outlook.com>
2025-05-13 12:20:06 -0300

    add nm-applet

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -1,4 +1,4 @@
-yay -S --needed --noconfirm nwg-look hyprpolkitagent hyprpaper waybar pavucontrol hyprshade udiskie hyprlock hypridle
+yay -S --needed --noconfirm nwg-look hyprpolkitagent hyprpaper waybar pavucontrol hyprshade udiskie hyprlock hypridle network-manager-applet
 
 hyprshade install
 systemctl --user enable --now hyprshader.timer
