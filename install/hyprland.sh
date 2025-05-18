1f66e54103419d2057426bac4fb881d9d1f7bfbb
Saull Brandão <saull@outlook.com>
2025-05-18 12:56:14 -0300

    migrate from hyprland to cosmic

--- install/hyprland.sh
+++
@@ -1,4 +0,0 @@
-yay -S --needed --noconfirm nwg-look hyprpolkitagent hyprpaper waybar pavucontrol hyprshade udiskie hyprlock hypridle network-manager-applet
-
-hyprshade install
-systemctl --user enable --now hyprshader.timer
--- install/theme.sh
+++
@@ -1,1 +0,0 @@
-yay -S --noconfirm --needed -S qt5ct kvantum breeze-icons breeze breeze-gtk qt6ct-kde
