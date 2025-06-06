36c96afa688c01f41c5ed69cb75ebdd5e3761f51
Saull Brandão <saull@outlook.com>
2025-06-06 12:14:59 -0300

    add docker

--- install/dev.sh
+++ install/dev.sh
@@ -1,4 +1,4 @@
-yay -S --needed --noconfirm php php-sqlite php-pgsql composer nodejs npm go
+yay -S --needed --noconfirm php php-sqlite php-pgsql composer nodejs-lts-jod npm go docker docker-compose
 
 # PHP config
 # Remove existing configuration files
@@ -6,5 +6,14 @@ sudo rm -f /etc/php/conf.d/sqlite.ini
 sudo rm -f /etc/php/conf.d/pgsql.ini
 
 # Create new configuration files
-echo -e "extension=sqlite3\nextension=pdo_sqlite" | sudo tee /etc/php/conf.d/sqlite.ini > /dev/null
-echo -e "extension=pgsql\nextension=pdo_pgsql" | sudo tee /etc/php/conf.d/pgsql.ini > /dev/null
+echo -e "extension=sqlite3\nextension=pdo_sqlite" | sudo tee /etc/php/conf.d/sqlite.ini >/dev/null
+echo -e "extension=pgsql\nextension=pdo_pgsql" | sudo tee /etc/php/conf.d/pgsql.ini >/dev/null
+
+# Give this user privileged Docker access
+sudo usermod -aG docker ${USER}
+
+# Start docker service
+sudo systemctl restart docker.service
+
+# Enable docker service
+sudo systemctl enable docker.service
